1de20393c31fc677d1e64a8a220073cfc7d052c7
Igor Rondon <igor.rondon3@outlook.com>
2024-07-20 06:58:02 -0400

    Commit 2

--- Tpos-variaveis/src/TiposVariaveis.java
+++ Tpos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,9 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //constantes sempre começam com a palavra reservada final e sempre em caps lock
+        final int ANO_NASCIMENTO_IGOR = 2006;
+        final int ANO_ALISTAMENTO_IGOR = 2024;
+
+        System.out.println("O ano de nascimento do Igor é "+ANO_NASCIMENTO_IGOR+ ", logo, seu ano de alistamento no serviço militar será em "+ANO_ALISTAMENTO_IGOR);
     }
 }
